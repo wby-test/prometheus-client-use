84661438c5d37dae0526e575067fafab794695aa
wangbaoyi1 <wangbaoyi1@jd.com>
2024-04-10 18:42:24 +0800

    (prometheus): client 基础库指标确认

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"go-prometheus/internal/metrics"
+	"time"
+
+	"go-prometheus/internal/metrics2"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+
 	gin.SetMode(gin.DebugMode)
 	router := gin.Default()
-	router.Use(gin.Logger(), metrics.CountAndDuration())
+	router.Use(gin.Logger(), metrics2.Metrics())
 	router.GET("/hello", func(context *gin.Context) {
 		context.Set("result: ", "hello")
 	})
+	router.GET("/flight", func(context *gin.Context) {
+		time.Sleep(1 * time.Second)
+		context.Set("result: ", "success")
+	})
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
-	metrics.AddHandleFunc(router)
+	//metrics.AddHandleFunc(router)
 
 	router.Run(":8088")
 }
--- internal/metrics2/prometheus.go
+++ internal/metrics2/prometheus.go
@@ -1,12 +1,10 @@
-package metrics
+// todo: prometheus-client 提供的指标
+package metrics2
 
 import (
-	"net/http"
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
@@ -15,11 +13,19 @@ import (
 
 var (
 	// serviceName 需要服务 deployment 含有 app 标签，或者通过 prometheus 标签操作，无需在此定义
-	serviceName        = os.Getenv("app")
-	commonLabels       = []string{"path", "code", "host", "method", "service_code"}
+	serviceName  = os.Getenv("app")
+	commonLabels = []string{"path", "code", "host", "method", "service_code"}
+
+	uptime = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "uptime",
+			Help: "HTTP service uptime.",
+		}, []string{"serviceName"},
+	)
+
 	http_request_total = promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "http_requests_total",
+			Name: "http_request_total",
 			Help: "The total number of processed http requests",
 		},
 		commonLabels,
@@ -39,7 +45,6 @@ var (
 			Name: "http_request_in_flight",
 			Help: "Current number of http requests in flight",
 		},
-
 		commonLabels,
 	)
 
@@ -48,29 +53,29 @@ var (
 			Name: "http_request_summary_seconds",
 			Help: "Summary of lantencies for HTTP requests",
 		},
-
 		commonLabels,
 	)
 )
 
-const (
-	metricsPath = "/metrics"
-	healthzPath = "/healthz"
-)
+//func Init() {
+//	prometheus.MustRegister(
+//		uptime,
+//		http_request_total,
+//		http_request_duration_milliseconds,
+//		http_request_in_flight,
+//		http_request_summary_seconds,
+//	)
+//
+//	go recordUptime()
+//}
 
-func AddHandleFunc(router *gin.Engine) {
-	router.GET(healthzPath, func(c *gin.Context) { c.Status(http.StatusOK) })
-	router.GET(metricsPath, prometheusHandler())
-}
-
-func prometheusHandler() gin.HandlerFunc {
-	h := promhttp.Handler()
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
+func recordUptime() {
+	for range time.Tick(time.Second) {
+		uptime.WithLabelValues(serviceName).Inc()
 	}
 }
 
-func CountAndDuration() gin.HandlerFunc {
+func Metrics() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		path := c.FullPath()
 		if path == metricsPath {
@@ -84,10 +89,16 @@ func CountAndDuration() gin.HandlerFunc {
 		host := c.RemoteIP()
 		// kuplus 服务使用
 		serviceCode := c.GetString("code")
-		http_request_total.WithLabelValues(path, code, host, method, serviceCode).Inc()
-		http_request_duration_milliseconds.WithLabelValues(path, code, host, method, serviceCode).Observe(float64(time.Since(now).Milliseconds()))
-		http_request_in_flight.WithLabelValues(path, code, host, method, serviceCode).Inc()
-		defer http_request_in_flight.WithLabelValues(path, code, host, method, serviceCode).Dec()
-		http_request_summary_seconds.WithLabelValues(path, code, host, method, serviceCode).Observe(time.Since(now).Seconds())
+		labels := []string{path, code, host, method, serviceCode}
+		http_request_total.WithLabelValues(labels...).Inc()
+		http_request_duration_milliseconds.WithLabelValues(labels...).Observe(float64(time.Since(now).Milliseconds()))
+		http_request_in_flight.WithLabelValues(labels...).Inc()
+		defer http_request_in_flight.WithLabelValues(labels...).Dec()
+		http_request_summary_seconds.WithLabelValues(labels...).Observe(time.Since(now).Seconds())
 	}
 }
+
+const (
+	metricsPath = "/metrics"
+	healthzPath = "/healthz"
+)
